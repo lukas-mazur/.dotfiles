f011c60ed47ebc277f9a2555b6e44ead8977f6b6
lukas-mazur <59335056+lukas-mazur@users.noreply.github.com>
2022-06-06 21:59:35 +0200

    modified install.sh

--- plugins/install.sh
+++ plugins/install.sh
@@ -18,24 +18,44 @@ mkdir bin
 bin_path=~/terminal_tools/bin ./direnv/install.sh
 
 
-echo """#!bin/bash
+cat >tools_env.sh <<'EOL'
+#!bin/bash
 
 ## tldr alias
-if [ -z \"$PYTHONUSERBAE\" ]; then
-    alias tldr=\"~/.local/bin/tldr\"
+if [ -z "$PYTHONUSERBAE" ]; then
+    alias tldr="~/.local/bin/tldr"
 else
-    alias tldr=\"\${PYTHONUSERBASE}/bin/tldr\"
+    alias tldr="${PYTHONUSERBASE}/bin/tldr"
 fi
 
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
-export PATH=\$PATH:~/terminal_tools/bin
+export PATH=$PATH:~/terminal_tools/bin
 
-eval "\$\(direnv hook bash\)"
-""" > tools_env.sh
+eval "$(direnv hook bash)"
+EOL
+
+
+
+cat >uninstall.sh <<'EOL'
+#!/bin/bash
+
+pip3 uninstall -y tldr
+
+
+sed -i "s|source \~\/terminal\_tools\/tools\_env\.sh||g" ~/.bashrc
+
+rm -rf ~/terminal_tools
+cd ~/
+""" > uninstall.sh
 
 echo "source ~/terminal_tools/tools_env.sh" >> ~/.bashrc
 
 cd $current_path
+EOL
+
+chmod +x uninstall.sh
+
+
 
--- plugins/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip3 uninstall -y tldr
-
-
-sed -i "s|source \~\/terminal\_tools\/tools\_env\.sh||g" ~/.bashrc
-
-rm -rf ~/terminal_tools
-cd ~/
